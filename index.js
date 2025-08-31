baec07fad93442588b7383413cdbfabf9b422a4b
Ajay <mrakmondal6612@gmail.com>
2025-08-31 19:24:13 +0530

    remove logs and commats

--- index.js
+++ index.js
@@ -170,6 +170,11 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
+    cookie: {
+      secure: process.env.NODE_ENV === 'production',
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000 // 24 hours
+    }
   })
 );
 
